4c679a26eed63291452f4eef4e5ab9eadedfeb60
Script-analysis <819124366@qq.com>
2026-04-09 20:44:43 +0800

    Add files via upload

---
+++ scRNA analysis/6.scRNAseq-TF and signaling pathway analysis.R
@@ -0,0 +1,10 @@
+#######################Transcription factor analysis
+#The code was obtained from its official GitHub repository (https://github.com/saezlab/DoRothEA).
+
+#######################Cell-cell communication analysis
+#The code was obtained from its official GitHub repository (https://github.com/sqjin/CellChat).
+
+#######################Analysis of 'gene set' activity 
+#step 1. The gene sets were downloaded from Molecular Signatures Database (MSigDB).
+#step 2. Identify cells with active gene sets in single-cell RNA-seq data.
+#The code was referenced from the AUCell Bioconductor package documentation (https://bioconductor.org/packages/release/bioc/html/AUCell.html).

4739cb8d8eb9690115eef284cbd52262cb564730
Script-analysis <819124366@qq.com>
2026-04-09 20:44:24 +0800

    Add files via upload

---
+++ Microarray analysis/8. Extraction of the gene expression matrix for ML (sAAA and lAAA).R
@@ -0,0 +1,21 @@
+setwd("C:\\AAA_analysis\\Control_AAA\\step6.Extraction of the gene expression matrix for ML")
+library(limma)
+library(dplyr)
+library(tibble)
+library(ggplot2)
+library(pheatmap)
+library(sva)
+genes="DMR_DEGs_18genes.txt"
+genes<-read.table(genes, header=T, sep="\t", check.names=F)
+file<-"GSE57691_normalizationt.txt"
+data<-read.table(file, header=T, sep="\t", check.names=F,row.names = 1)
+genes <- genes$ID
+genes_use <- intersect(genes, rownames(data))
+data_sub <- data[genes_use, ,drop = FALSE]
+write.table(data_sub,file="GSE57691_18genes.txt",sep="\t",quote=F,col.names=T,row.names = T)
+file<-"GSE7084_normalizationt.txt"
+data<-read.table(file, header=T, sep="\t", check.names=F,row.names = 1)
+genes <- genes$ID
+genes_use <- intersect(genes, rownames(data))
+data_sub <- data[genes_use, ,drop = FALSE]
+write.table(data_sub,file="GSE7084_18genes.txt",sep="\t",quote=F,col.names=T,row.names = T)
